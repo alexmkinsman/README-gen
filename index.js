2f01e4618570585a5f01fd4bfc4d3ddfd9a1d29c
Alexandra Kinsman <alexandrakinsman@Alexs-Mac.lan>
2022-09-22 14:08:34 -0700

    questions written

--- index.js
+++ index.js
@@ -7,18 +7,62 @@ const path = require("path");
 const questions = [
   {
     type: "input",
-    name: "github",
-    message: "Enter your GitHub username.",
+    name: "project",
+    message: "Enter your project's title.",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Write a short description of your project.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Provide the command to be used for installation.",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Provide the command to be used to run tests.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide any instructions regarding use of the repository.",
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "Provide information regarding contributing to the repository.",
   },
   {
     type: "input",
     name: "github",
     message: "Enter your GitHub username.",
   },
+  
+  {
+    type: "list",
+    name: "license",
+    message: "Choose the license which best fits your project.",
+    choices: ["MIT", "Apache", "Boost"]
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub username.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address.",
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync()
+}
 
 // TODO: Create a function to initialize app
 function init() {}
